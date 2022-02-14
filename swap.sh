036054b4fd470dd3d491ee5439f4b2197bdf0fde
Abdul Rahman Ajeena <96892118+AbdulRahmanAjeena@users.noreply.github.com>
2022-02-14 16:11:14 -0500

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,23 +1,12 @@
 swapoff /swapfile
-
-free -h
-
+free -h;
 sudo fallocate -l 60G /swapfile
-
 sudo chmod 600 /swapfile
-
-mkswap /swapfile
-
-swapon /swapfile
-
-echo '/swapfile none swap sw 00' | sudo tee -a /etc/fstab
-
-free -h
-
-apt update
-
-apt install neofetch
-
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 00' | sudo tee -a /etc/fstab;
+free -h;
+sudo apt update
+sudo apt install neofetch
 neofetch
-
-apt install glances
+sudo apt install glances
